73710cb6beb391a7d5f87b6117f717546f0b24a4
AldiNFitrah <52171676+AldiNFitrah@users.noreply.github.com>
2022-03-06 16:33:54 +0700

    [CHORES] setup test env with sqlite (#4)
    * [CHORES] change badaso module test discovery to only match *Test.php files
    
    * [CHORES] add APP_KEY to phpunit.xml
    
    * [CHORES] add testing database connection config
    
    * [CHORES] chmod all files to 777
    
    * [CHORES] setup to create a fresh in-memory db every time we run a test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('migrate:fresh', ['--database' => 'testing']);
+        $this->artisan('db:seed', ['--class' => "Database\Seeders\Badaso\BadasoSeeder", '--database' => 'testing']);
+
+        $this->app['config']->set('badaso.database.prefix', 'badaso_');
+        $this->app['config']->set('database.default', 'testing');
+    }
 }
